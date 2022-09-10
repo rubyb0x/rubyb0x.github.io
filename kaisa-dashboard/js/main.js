5fc1aa4b7d065b7ab4e8aac6068c9dd36e438209
Ruby Magturo <ruby.magturo@gmail.com>
2022-09-10 11:58:15 +0800

    adding revised index and mainjs

--- kaisa-dashboard/js/main.js
+++ kaisa-dashboard/js/main.js
@@ -1,7 +1,5 @@
 //data base_url
-//var url = 'https://rubyb0x.github.io/kaisa-dashboard/data/bndry_basilan_OB2019.geojson';
-var url = 'https://rubyb0x.github.io/kaisa-dashboard/data/basilan_ob2019.json';
-//var url = 'data/boundaries_barmm_muni_OB2019.geojson';
+var url = 'https://rubyb0x.github.io/kaisa-dashboard/data/map.geojson';
 
 //map options
 var mapOptions = {
@@ -12,47 +10,8 @@ var mapOptions = {
 var map = L.map('map', mapOptions);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// set style function that sets fill color property
-function style(feature) {
-    return {
-        fillColor: 'green',
-        fillOpacity: 0.5,
-        weight: 2,
-        opacity: 1,
-        color: '#ffffff',
-        dashArray: '3'
-    };
-}
-	var highlight = {
-		'fillColor': 'yellow',
-		'weight': 2,
-		'opacity': 1
-	};
-
-// add popup description for each feature
-function forEachFeature(feature, layer) {
-
-        var popupContent = "<p><b>Region: </b>"+ feature.properties.Reg_Name +
-            "</br>Province: "+ feature.properties.Pro_Name +
-            "</br>Municipality: "+ feature.properties.Mun_Name + '</p>';
-
-        layer.bindPopup(popupContent);
-
-        layer.on("click", function (e) {
-            munLayer.setStyle(style); //resets layer colors
-            layer.setStyle(highlight);  //highlights selected.
-        });
-}
-
-// null variable to hold tileLayer
-var munLayer = L.geoJson(null, {onEachFeature: forEachFeature, style: style});
-
-	$.getJSON(url, function(data) {
-        munLayer.addData(data);
-    });
-
- munLayer.addTo(map);
+var geojsonLayer = new L.GeoJSON.AJAX(url);
+geojsonLayer.addTo(map);
